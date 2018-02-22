8c56840efcfaa186279f9aee2d6fe8ce91977f1f
Jiyang Wang <jiyang.wang.us@gmail.com>
2018-02-21 22:26:39 -0800

    Feb.21, 2018

--- LC7-ReverseInteger.js
+++ LC7-ReverseInteger.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var reverse = function(x) {
-  var str = x.toString();
-  if (str[0] === '-') {
-      res = +(str[0] + str.substring(1).split('').reverse().join(''));
-  } else {
-      res = +(str.split('').reverse().join(''));
-  }
-  if (res > Math.pow(2, 31)  - 1 || res < -1 * Math.pow(2, 31)) {
-      return 0;
-  } else {
-      return res;
-  }
+    var res = 0;
+    while (x !== 0) {
+        res = res * 10 + x % 10;
+        x = x > 0 ? Math.floor(x / 10) : Math.ceil(x / 10);
+    }
+    if (res > Math.pow(2, 31)  - 1 || res < -1 * Math.pow(2, 31)) {
+        return 0;
+    } else {
+        return res;
+    }
 };
 
 var x = -123;

0c4099b920d339dfabcbb81562669dac3851ffb5
Jiyang Wang <jiyang.wang.us@gmail.com>
2018-03-09 23:51:08 -0800

    Mar.9, 2018

---
+++ LC35-SearchInsertPosition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  if (!nums || nums.length === 0) {
+      return 0;
+  }
+  var left = 0, right = nums.length - 1;
+  while (left <= right) {
+      var mid = Math.floor((left + right) / 2);
+      if (nums[mid] < target) {
+          left = mid + 1;    
+      } else {
+          right = mid - 1;
+      }
+  }
+  return left;
+};
